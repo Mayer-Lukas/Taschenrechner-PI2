7aafb51097be4550743690fc14bccdd9f2edc205
Lukas <mayerlukas.13.9.2005@gmail.com>
2025-06-03 11:55:55 +0200

    Konstanten für die Grundlogik

---
+++ src/Taschenrechner/model/BinaryOperation.java
@@ -0,0 +1,18 @@
+package Taschenrechner.model;
+
+public class BinaryOperation implements Expression {
+    private final Expression left;
+    private final Expression right;
+    private final Operator operator;
+
+    public BinaryOperation(Expression left, Expression right, Operator operator) {
+        this.left = left;
+        this.right = right;
+        this.operator = operator;
+    }
+
+    @Override
+    public double evaluate() {
+        return operator.apply(left.evaluate(), right.evaluate());
+    }
+}
