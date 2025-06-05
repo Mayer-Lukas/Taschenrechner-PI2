aa07ba9df50bc2367fd57ad376fc9052ecd5c7d8
Lukas <mayerlukas.13.9.2005@gmail.com>
2025-06-05 11:00:06 +0200

    div 0 eingefügt in Division.java

--- src/Taschenrechner/model/Division.java
+++ src/Taschenrechner/model/Division.java
@@ -1,8 +1,11 @@
 package Taschenrechner.model;
 
-public class Division implements Operator{
+public class Division implements Operator {
     @Override
-    public double apply(double a, double b) {
-        return a / b;
+    public double apply(double left, double right) {
+        if (right == 0) {
+            throw new ArithmeticException("Division durch 0");
+        }
+        return left / right;
     }
 }
